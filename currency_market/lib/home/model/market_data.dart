45ae3769c1cc6bf19ee5895e590b46cf6d16e4e7
Tolga Kartal <tolga@devideal.com>
2022-10-07 07:04:59 +0200

    Current market project initialized

---
+++ currency_market/lib/home/model/market_data.dart
@@ -0,0 +1,92 @@
+class MarketData {
+  Transaction? transaction;
+
+  MarketData({this.transaction});
+
+  MarketData.fromJson(Map<String, dynamic> json) {
+    transaction = json['transaction'] != null
+        ? Transaction.fromJson(json['transaction'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (transaction != null) {
+      data['transaction'] = transaction?.toJson();
+    }
+    return data;
+  }
+}
+
+class Transaction extends Comparable<Transaction> {
+  final String? base;
+  final String? quote;
+  final String? type;
+  final double? lastPrice;
+  final double? volume;
+
+  Transaction({
+    required this.base,
+    required this.quote,
+    required this.type,
+    required this.lastPrice,
+    required this.volume,
+  });
+
+  Transaction.fromJson(
+    Map<String, dynamic> json,
+  )   : base = json['base'],
+        quote = json['quote'],
+        type = json['type'],
+        lastPrice = json['lastPrice'],
+        volume = json['volume'];
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['base'] = base;
+    data['quote'] = quote;
+    data['type'] = type;
+    data['lastPrice'] = lastPrice;
+    data['volume'] = volume;
+    return data;
+  }
+
+  @override
+  int compareTo(other) {
+    final otherVal = _sortPriorityValue(other.base);
+    final currentVal = _sortPriorityValue(base);
+    final otherQuote = _sortQuoteValue(other);
+    final currentQuote = _sortQuoteValue(this);
+
+    return (otherVal + otherQuote) - (currentVal + currentQuote);
+  }
+
+  int _sortPriorityValue(String? val) {
+    if (val == null) {
+      return 0;
+    } else if (val == 'BTC') {
+      return 40;
+    } else if (val == 'ETH') {
+      return 30;
+    } else if (val == 'WOO') {
+      return 20;
+    } else {
+      return 10;
+    }
+  }
+
+  int _sortQuoteValue(Transaction? val) {
+    if (val == null) {
+      return 0;
+    }
+    if (val.quote == 'USDT') {
+      return 3;
+    } else if (val.quote == 'USDC') {
+      return 2;
+    } else if (val.type == 'FUTURES') {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
---
+++ currency_market/lib/home/presentation/home.dart
@@ -0,0 +1,85 @@
+import 'dart:ui';
+import 'package:currency_market/home/presentation/transaction_table.dart';
+import 'package:currency_market/home/state/market_state.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final marketDataProvider = context.watch<MarketState>();
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size(MediaQuery.of(context).size.width, 40),
+            child: ClipRRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              ),
+            ),
+          ),
+          extendBodyBehindAppBar: true,
+          body: Column(
+            children: [
+              const SizedBox(height: 25),
+              SizedBox(
+                height: 50,
+                child: TabBar(
+                  labelStyle: const TextStyle(fontSize: 18),
+                  indicatorColor: Colors.amberAccent,
+                  indicatorWeight: 2.0,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  labelColor: Colors.black,
+                  labelPadding: const EdgeInsets.only(top: 8),
+                  unselectedLabelColor: Colors.black,
+                  isScrollable: false,
+                  dragStartBehavior: DragStartBehavior.start,
+                  mouseCursor: SystemMouseCursors.noDrop,
+                  onTap: (x) {
+                    marketDataProvider.selectedMarketDataType = x == 0
+                        ? MarketDataType.all
+                        : x == 1
+                            ? MarketDataType.spot
+                            : MarketDataType.future;
+                  },
+                  tabs: const [
+                    Tab(text: 'ALL'),
+                    Tab(text: spotTypeKey),
+                    Tab(text: futureTypeKey),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TabBarView(
+                    children: [
+                      Center(
+                        child: Column(
+                          children: const [
+                            SizedBox(height: 64),
+                            TransactionTableHeader(),
+                            SizedBox(height: 16),
+                            TransactionTable(),
+                          ],
+                        ),
+                      ),
+                      const Center(child: Text(spotTypeKey)),
+                      const Center(child: Text(futureTypeKey)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
